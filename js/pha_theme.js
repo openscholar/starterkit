91782cc0b31283d5b4007230e9073af4254717b8
Jamie Malcolm-Brown <jamie@curoergosum.com>
2016-02-26 14:42:08 -0500

    updates

--- js/pha_theme.js
+++ js/pha_theme.js
@@ -1,20 +1,3 @@
 jQuery(document).ready(function($) {
-	var num = 184;
-	//number of pixels before modifying styles
-
-	jQuery(window).bind('scroll', function() {
-		if (jQuery(window).scrollTop() > num) {
-			jQuery('#menu-bar').addClass('fixed');
-		} else {
-			jQuery('#menu-bar').removeClass('fixed');
-		}
-	});
-
-	jQuery('.front .block-boxes-os_sv_list_box').each(function() {
-		var $this = $(this);
-		var count = jQuery('.node', $this).length;
-
-		jQuery($this).addClass('lopz-' + count);
-	});
-
+	
 });
